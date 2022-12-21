cd0c4fd79dfb2343ae2d00a62882e7d2306c0dac
reverbePrintemps <59572064+reverbePrintemps@users.noreply.github.com>
2022-12-21 19:20:08 +0100

    Global styles and project assets
    Packages

---
+++ src/styles/App.ts
@@ -0,0 +1,31 @@
+export const globalStyles = {
+  font: {
+    family: {
+      regular: "Roboto Regular",
+      medium: "Roboto Medium",
+      bold: "Roboto Bold",
+    },
+    size: {
+      large: "24px",
+      regular: "16px",
+      small: "12px",
+    },
+    lineHeight: {
+      large: "30px",
+      regular: "22px",
+      small: "16px",
+    },
+    color: {
+      "primary--raisin-black": "#202125",
+      "secondary--quicksilver-grey": "#a1a2a4",
+      "highlight--lime-green": "#5bcb02",
+    },
+  },
+  background: {
+    "card--paper-white": "#ffffff",
+    "app--cultured-grey-light": "#f8f8f8",
+    "shadow--cultured-grey": "#eeeeee",
+  },
+};
+
+};
